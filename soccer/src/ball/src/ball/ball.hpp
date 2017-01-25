c8c684f37389061406ceb65f4fb1c7420b012774
Chris Chan <ctf.chan@mail.utoronto.ca>
2017-01-24 19:44:42 -0500

    Added the Header file for field constants

--- soccer/src/ball/src/ball/ball.hpp
+++ soccer/src/ball/src/ball/ball.hpp
@@ -8,15 +8,21 @@
 #ifndef BALL_SRC_BALL_HPP_
 #define BALL_SRC_BALL_HPP_
 
-#define BALL_SIZE 3
 #define PI 3.14159826
+
+#define BALL_SIZE 3
+#define BALL_DIAMETER			130
+#define BALL_RADIUS				21 	// 20.6
+#define BALL_WEIGHT				130
+#define BALL_COLOR				WHITE
+
 
 class Ball {
 private:
 	int circumference();
 
 public:
-	const int radius = 93;	// Radius in millimeters
+	static const int radius = BALL_RADIUS;	// Radius in millimeters
 
 	Ball();
 	~Ball();
---
+++ soccer/src/field_mapper/src/soccerfield.cpp
@@ -0,0 +1,18 @@
+/*
+ * soccerfield.cpp
+ *
+ *  Created on: Jan 24, 2017
+ *      Author: soccer
+ */
+
+#include "soccerfield.hpp"
+
+SoccerField::SoccerField() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SoccerField::~SoccerField() {
+	// TODO Auto-generated destructor stub
+}
+
