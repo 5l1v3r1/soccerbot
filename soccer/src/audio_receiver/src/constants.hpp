004aa97c6b0b37fa3063cc58ef896e06c0e073f1
Chris Chan <ctf.chan@mail.utoronto.ca>
2017-01-24 21:56:25 -0500

    My 26th commit by myself

---
+++ soccer/src/audio_receiver/src/constants.hpp
@@ -0,0 +1,12 @@
+/*
+ * constants.hpp
+ *
+ *  Created on: Jan 24, 2017
+ *      Author: soccer
+ */
+
+#ifndef AUDIO_RECEIVER_SRC_CONSTANTS_HPP_
+#define AUDIO_RECEIVER_SRC_CONSTANTS_HPP_
+
+
+#endif /* AUDIO_RECEIVER_SRC_CONSTANTS_HPP_ */
