b440697d888c91ed40f778232b90ac7280f57c06
mikesorocky <mikesorocky@gmail.com>
2017-01-29 11:28:59 -0800

    Added main.cpp, stuff for CMake

---
+++ soccer/src/control/build/CMakeFiles/agentspark.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/agentspark.dir/main.cpp.o"
+  "agentspark.pdb"
+  "agentspark"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/agentspark.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ soccer/src/control/main.cpp
@@ -0,0 +1,30 @@
+#include "iostream"
+
+
+
+
+void printGreeting() {
+
+	std::cout << "UTRA FC Code Base" << std::endl;
+
+	return;
+}
+
+void readOptions() {
+
+	return;
+}
+
+int main(int argc, char * argv[]) {
+
+
+
+	printGreeting();
+	readOptions();
+
+
+
+
+
+	return 0;
+}
