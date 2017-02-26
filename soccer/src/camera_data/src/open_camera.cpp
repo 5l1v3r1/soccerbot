336944a5d7b75f7c5bcaf74a8ecf6773977f1f4c
Jason Wang <vuwij@me.com>
2017-02-25 22:32:53 -0500

    Got Edge Detection Working

--- soccer/src/camera_data/src/open_camera.cpp
+++ soccer/src/camera_data/src/open_camera.cpp
@@ -1,29 +1,63 @@
-#include <stdio.h>
+#include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
-//#include <opencv2/opencv.hpp>
+#include <iostream>
 
+using namespace std;
 using namespace cv;
 
-int main(int argc, char** argv )
+class cam_test{
+  public:
+    cam_test(){
+      VideoCapture cap(CV_CAP_ANY); // open the video camera no. 0
+
+         if (!cap.isOpened())  // if not success, exit program
+         {
+             cout << "Cannot open the video cam" << endl;
+         }
+
+        double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+        double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+
+         cout << "Frame size : " << dWidth << " x " << dHeight << endl;
+
+         namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
+
+         while (1)
+         {
+             Mat frame;
+
+             bool bSuccess = cap.read(frame); // read a new frame from video
+
+              if (!bSuccess) //if not success, break loop
+             {
+                  cout << "Cannot read a frame from video stream" << endl;
+                  break;
+             }
+			
+			imshow("MyVideo", frame);
+
+             if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+            {
+                 cout << "esc key is pressed by user" << endl;
+                 break;
+            }
+         }
+
+
+    }
+
+    ~cam_test(){
+        cvDestroyWindow("Camera_Output"); //Destroy Window
+    }
+
+};
+
+int main(int argc, char **argv)
 {
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
+  // Set up ROS.
+  ros::init(argc, argv, "internal_cam_test");
+  cam_test cam_object;
 
-    Mat image;
-    image = imread( argv[1], 1 );
-
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
-    return 0;
+  ROS_INFO("Cam Tested!");
 }
