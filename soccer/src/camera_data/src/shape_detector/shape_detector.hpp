52935be8185e1b3a4513e5ec2e5337cb9730069f
Chris Chan <ctf.chan@mail.utoronto.ca>
2017-03-05 15:57:36 -0500

    Added circle detector library

---
+++ soccer/src/camera_data/src/shape_detector/shape_detector.hpp
@@ -0,0 +1,16 @@
+#ifndef SHAPE_DETECTOR_SHAPE_DETECTOR_
+#define SHAPE_DETECTOR_SHAPE_DETECTOR_
+
+class ShapeDetector {
+private:
+
+public:
+
+	ShapeDetector();
+	~ShapeDetector();
+
+};
+
+
+#endif /* SHAPE_DETECTOR_SHAPE_DETECTOR_ */
+
