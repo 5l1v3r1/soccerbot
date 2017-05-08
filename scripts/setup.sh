83f08ec04ab8a1819ac8db140a15178ae6e3ec7f
Fyyaz <fyyazk@gmail.com>
2017-05-08 19:46:10 -0400

    fixed install script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,9 +11,20 @@ sudo service restart ssh
 
 sudo apt-get install xfce4 xfce4-goodies tightvncserver
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-echo "#!/bin/bash" >> ~/.vnc/xstartup
-echo "xrdb $HOME/.Xresources" >> ~/.vnc/xstartup
-echo "startxfce4 &" >> ~/.vnc/xstartup
+mv xstartup ~/.vnc/xstartup
 
 #### ROS ####
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+source /opt/ros/kinetic/setup.bash
+echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
+source ~/.zshrc
+sudo apt-get install python-rosinstall
+
 
