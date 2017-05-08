15780f74590c43a6dbe560fb2304f55a28684d0e
Fyyaz <fyyazk@gmail.com>
2017-05-08 19:51:15 -0400

    added -y to script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,11 +5,11 @@
 sudo apt-get update
 
 #### Basic requirements ####
-sudo apt install openssh-server
+sudo apt install openssh-server -y
 sudo systemctl restart sshd.service
 sudo service restart ssh
 
-sudo apt-get install xfce4 xfce4-goodies tightvncserver
+sudo apt-get install xfce4 xfce4-goodies tightvncserver -y
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 mv xstartup ~/.vnc/xstartup
 
@@ -17,7 +17,7 @@ mv xstartup ~/.vnc/xstartup
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
-sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-desktop-full -y
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
@@ -25,6 +25,6 @@ source ~/.bashrc
 source /opt/ros/kinetic/setup.bash
 echo "source /opt/ros/kinetic/setup.zsh" >> ~/.zshrc
 source ~/.zshrc
-sudo apt-get install python-rosinstall
+sudo apt-get install python-rosinstall -y
 
 
