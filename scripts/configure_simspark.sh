99335b675edd9d94efe8cd78159350ddf0bd8864
Jason Wang <vuwij@me.com>
2017-05-09 20:55:36 -0400

    Revert "Updated simspark"
    This reverts commit ddf05863549bd10b72942036fdbeff462c8d85c4.

--- scripts/configure_simspark.sh
+++ scripts/configure_simspark.sh
@@ -1,14 +1,9 @@
-sudo apt-add-repository ppa:gnurubuntu/rubuntu -y
-sudo apt-get update
-sudo apt-get install rcssserver3d -y
-
 sudo apt-get install g++ subversion cmake libfreetype6-dev libode-dev libsdl-dev ruby ruby-dev libdevil-dev libboost-dev libboost-thread-dev libboost-regex-dev libboost-system-dev qt4-default -y
 sudo apt-get install tetex-extra latex2html imagemagick -y
 sudo apt-get install doxygen -y
 
-cd ../
-svn co https://svn.code.sf.net/p/simspark/svn/trunk simspark
-cd simspark
+# Simspark
+cd ../simspark/
 svn update
 cd spark
 mkdir build
