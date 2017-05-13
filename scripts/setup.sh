b00b72cd72eeddba1187b05538ce53e99b43e82c
root <root@localhost.localdomain>
2017-05-13 00:54:59 -0400

    Fixed VNC script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,8 +10,8 @@ sudo systemctl restart sshd.service
 sudo service ssh start
 
 sudo apt-get install xfce4 xfce4-goodies tightvncserver -y
-mv -f ~/.vnc/xstartup ~/.vnc/xstartup.bak
-cp xstartup ~/.vnc/xstartup
+cp -r .vnc  ~/.vnc
+chmod +x ~/.vnc/xstartup
 
 #### ROS ####
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
