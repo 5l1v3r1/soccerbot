3445982a53db14563c8668219202714ccecfe4f9
Justin-Yuan <justin.zcyuan@gmail.com>
2017-06-19 20:39:57 -0400

    Fixed pizza

--- soccer/devel/share/robot_localization/cmake/robot_localization-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(robot_localization_MESSAGE_FILES "")
-set(robot_localization_SERVICE_FILES "/home/vuwij/soccerbot/soccer/src/robot_localization/srv/SetPose.srv;/home/vuwij/soccerbot/soccer/src/robot_localization/srv/SetDatum.srv")
