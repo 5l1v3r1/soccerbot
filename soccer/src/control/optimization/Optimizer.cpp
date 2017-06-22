2071ae11197930c149a95a3cbc4150a55fc0ae80
colorzzr <colorzzr@gmail.com>
2017-06-22 08:19:08 -0700

    leave comment

--- soccer/src/control/optimization/Optimizer.cpp
+++ soccer/src/control/optimization/Optimizer.cpp
@@ -1,10 +1,12 @@
 //cpp file for checking the robot is fallen
 #include "Optimizer.h"
 
+//change to constructor later
 Optimizer::Optimizer(NaoBehaviour*& behaviour_) {
     behaviour = behaviour_;
 }
 
+//destructor
 Optimizer::~Optimizer() {
     delete behaviour;
 }
